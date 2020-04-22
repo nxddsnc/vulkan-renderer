dd7065a2a10afbc369004cc404db6e8e67b74d08
unknown <fanqilei@corp.netease.com>
2020-04-22 16:21:15 +0800

    stash

--- vulkan-renderer/MyScene.h
+++ vulkan-renderer/MyScene.h
@@ -15,6 +15,8 @@ public:
     void AddDrawable(std::shared_ptr<Drawable> node);
     std::vector<std::shared_ptr<Drawable>> GetDrawables();
 
+	void AddAnimation(std::shared_ptr<MyAnimation> animation);
+
 	BBox	m_bbox;
 private:
     std::vector<std::shared_ptr<Drawable>> _drawables;
